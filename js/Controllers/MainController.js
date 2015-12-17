32ee45d3946899399f86543d4c9a869cbd261292
csafreen <csafreen@gmail.com>
2015-12-17 20:41:40 +0530

    Home Laptop

---
+++ js/Controllers/MainController.js
@@ -0,0 +1,29 @@
+/**
+ * Created by AFREEN on 29-11-2015.
+ */
+app.controller('MainController',['$scope','Users',function($scope,Users) {
+    $scope.title= 'Personal Details';
+    $scope.AllUsers=Users;
+    $scope.CurrentUser=Users.UserDetails[0];
+    $scope.doThis=function(id){
+        var i=0;
+
+        for( i=0;i<$scope.AllUsers.UserDetails.length; i++)
+        {
+            if($scope.AllUsers.UserDetails[i].id == id)
+            {
+                $scope.CurrentUser=$scope.AllUsers.UserDetails[i];
+            }
+        }
+    };
+    $scope.Edit=true;
+    $scope.editUserDetails=function(id){
+        window.location="EditUser.html"+id;
+    }
+}]);
+
+app.controller('EditController',['$scope','Users',function($scope,Users){
+    $scope.CurrentUserDetail=Users;
+    $scope.user=$scope.CurrentUserDetail.UserDetails[0];
+}])
+
---
+++ js/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by AFREEN on 29-11-2015.
+ */
+var app=angular.module("myApp",[]);
